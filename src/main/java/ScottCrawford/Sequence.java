24dc02cdaaad08521e77540ef9a3350372e71d10
Scott Crawford <sfc17@pitt.edu>
2017-10-23 15:33:03 -0400

    Stuff

--- src/main/java/ScottCrawford/Sequence.java
+++ src/main/java/ScottCrawford/Sequence.java
@@ -1,6 +1,17 @@
 package ScottCrawford;
 
 public class Sequence{
+	public static int Tri(int value){
+		int tri=0;
+		for(int i=value;i>0;i--){
+			tri+=i;
+		}
+		return tri;
+	}
+	public static int Lazy(int value){
+		return ((value*value) + value + 2)/2;
+	}
+	
 	public static void main(String[] args) {
 		if (args == null || args.length == 0) {
 			System.out.println("Enter something buddy");
@@ -8,11 +19,8 @@ public class Sequence{
 		} 
 		else {
 			int value = Integer.parseInt(args[0]);
-			int tri=0;
-			for(int i=value;i>0;i--){
-				tri+=i;
-			}
-			lazy = ((value*value) + value + 2)/2;
+			int tri=Tri(value);
+			int lazy=Lazy(value);
 			System.out.println("Tri("+value+") = "+tri);
 			System.out.println("Lazy("+value+") = "+lazy);
 			System.exit(0);
