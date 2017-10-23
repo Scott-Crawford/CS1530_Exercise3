f6a2f9e981f921bff4e5d926ba4c53fee3aca13f
Scott Crawford <sfc17@pitt.edu>
2017-10-23 15:43:18 -0400

    fix method names

--- src/test/java/ScottCrawford/SequenceTest.java
+++ src/test/java/ScottCrawford/SequenceTest.java
@@ -6,32 +6,32 @@ import ScottCrawford.Sequence;
 public class SequenceTest {
 
     @Test
-    public void testSequence0() {
+    public void testSequenceTri0() {
         assertEquals(Sequence.Tri(0), 0);
     }
 
     @Test
-    public void testSequence1() {
+    public void testSequenceTri1() {
         assertEquals(Sequence.Tri(1), 1);
     }
 
     @Test
-    public void testSequence5() {
+    public void testSequenceTri5() {
         assertEquals(Sequence.Tri(5), 15);
     }
 
     @Test
-    public void testSequence0() {
+    public void testSequenceLazy0() {
         assertEquals(Sequence.Lazy(0), 0);
     }
 
     @Test
-    public void testSequence1() {
+    public void testSequenceLazy1() {
         assertEquals(Sequence.Lazy(1), 2);
     }
 
     @Test
-    public void testSequence5() {
+    public void testSequenceLazy5() {
         assertEquals(Sequence.Lazy(5), 16);
     }
 
